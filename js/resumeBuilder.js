ee7fe48480ac844327c26f79b30076eccf7feb78
peng-ling <paulengling@Pauls-MacBook-Air.local>
2015-10-18 14:49:49 +0200

    added first lines to resumeBuilder, learned: var, replace, difference between appende and prepend

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var Name = "Paul Engling";
+var Role = "Dipl. Ing.";
+
+var formattedName = HTMLheaderName.replace("%data%", Name);
+var formattedRole = HTMLheaderRole.replace("%data%", Role);
+
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedRole);
